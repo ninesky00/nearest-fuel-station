c20ed26d24dbe086efd33666e317b77e481c7b2c
ninesky00 <jiangzho@gmail.com>
2021-02-26 10:16:59 -0600

    initial setup and add tests for selector and redirects to search index

--- spec/features/user_can_visit_welcome_page_spec.rb
+++ spec/features/user_can_visit_welcome_page_spec.rb
@@ -7,4 +7,18 @@ describe "user can visit the welcome page" do
     expect(page).to have_content("Search For The Nearest Electric Charging Station")
     expect(page).to have_button("Find Nearest Station")
   end
+
+  it "has a selector for location" do 
+    visit '/'
+
+    expect(page).to have_select('location')
+  end
+
+  it "can select turing as location and be redirected to search index" do 
+    visit '/'
+
+    select('Turing', from: 'location')
+    click_button 'Find Nearest Station'
+    expect(current_path).to eq(search_path)
+  end
 end
