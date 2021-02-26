4bb580ad49fababd0b8524c0f3e709b5b41ab83a
ninesky00 <jiangzho@gmail.com>
2021-02-26 13:07:21 -0600

    change incorrect nested format for directions, incomplete implementation as it should iterate

--- app/poros/direction.rb
+++ app/poros/direction.rb
@@ -2,7 +2,7 @@ class Direction
   attr_reader :distance, :travel_time, :directions
   def initialize(data)
     @distance = data[:distance]
-    @travel_time = data[:formattedTime]
-    @directions = data[:legs][:maneuvers][:narrative]
+    @travel_time = (data[:formattedTime])
+    @directions = data[:legs].first[:maneuvers].first[:narrative]
   end
 end
